2cb12d4bafb972ced5059b12bec4ab288a22309c
Alexander Calugari <afishr00@gmail.com>
2019-09-18 22:32:12 +0300

    Add labI/taskII

---
+++ src/lab2/task2/Queue.java
@@ -0,0 +1,75 @@
+package lab2.task2;
+
+public class Queue
+{
+	private int list[];
+	private int elementsNumber = 0;
+	private int maxSize;
+	
+	public Queue()
+	{
+		this.maxSize = 999999; // Magic constant; Just hope you won't store 1m numbers in my queue;
+		list = new int[maxSize];
+	}
+	
+	public Queue(int maxSize)
+	{
+		this.maxSize = maxSize;
+		list = new int[maxSize];
+	}
+	
+	public void push(int value)
+	{
+		if (elementsNumber + 1 > maxSize)
+			return;
+		
+		for (int i = elementsNumber - 1; i >= 0; i--)
+		{
+			list[i+1] = list[i];
+		}
+		
+		elementsNumber++;
+		list[0] = value;
+	}
+	
+	public int pop()
+	{
+		if (elementsNumber == 0)
+			return 0;
+		
+		int value = list[0];
+		
+		for (int i = 0; i < elementsNumber; i++)
+		{
+			list[i] = list[i+1];
+		}
+		
+		elementsNumber--;
+		return value;
+	}
+	
+	public void listElements()
+	{
+		for (int i = 0; i < elementsNumber; i++)
+		{
+			System.out.print(list[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public boolean isEmpty()
+	{
+		if (elementsNumber == 0)
+			return true;
+		
+		return false;
+	}
+	
+	public boolean isFull()
+	{
+		if (elementsNumber == maxSize)
+			return true;
+		
+		return false;
+	}
+}
