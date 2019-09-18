5210d2810483535fa1493ba80d8fbde017202e9a
Alexander Calugari <afishr00@gmail.com>
2019-09-18 21:03:39 +0300

    Add labI/taskI

--- src/lab2/task1/Main.java
+++ src/lab2/task1/Main.java
@@ -5,6 +5,17 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-	
+		Box box1 = new Box();
+		Box box2 = new Box(3);
+		Box box3 = new Box(3, 2, 1);
+		
+		System.out.println(box1.getVolume());
+		System.out.println(box1.getSurface());
+		System.out.println();
+		System.out.println(box2.getVolume());
+		System.out.println(box2.getSurface());
+		System.out.println();
+		System.out.println(box3.getVolume());
+		System.out.println(box3.getSurface());
 	}
 }
